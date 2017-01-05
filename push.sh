7aac25926dae16043c5d6d234341687cb89f29d2
Peeter Joot <peeter.joot@lzlabs.com>
2017-01-05 11:09:51 -0500

    new file:   METADATA new file:   notes new file:   pull.sh new file:   push.sh

---
+++ push.sh
@@ -0,0 +1,3 @@
+repos="curvilinear GAelectrodynamics ece1229-antenna ece1229-antenna-figures figures figures.gabook frequencydomain gabookI gabookII gapauli latex stokesTheorem"
+for i in $repos ; do (cd $i && git commit -a) ; done
+for i in $repos ; do (cd $i && git push) ; done
